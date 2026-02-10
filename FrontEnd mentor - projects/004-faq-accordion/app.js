babdda11101b313d4305da9caca2d5da466d8940
DanijelBasaric <120591525+DanijelBasaric@users.noreply.github.com>
2026-02-10 10:32:58 +0100

    Add files via upload

---
+++ FrontEnd mentor - projects/004-faq-accordion/app.js
@@ -0,0 +1,20 @@
+const accordion = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+        console.log(this);
+
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
